74e8feabfddeb6e968bcd8d619abbc52f3b6182c
chsu00 <hyunsu@quantumuniverse.co>
2022-09-02 16:13:52 +0900

    Initial Commit

---
+++ Shared/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  Shared
+//
+//  Created by 최현수 on 2022/09/02.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
